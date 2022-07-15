e3753aaa21f297642c8e0c5eadbf6b2883e14ff0
Matthew Batchelder <borkweb@gmail.com>
2022-07-15 14:36:00 -0400

    Initial version

---
+++ src/WPTables/Container.php
@@ -0,0 +1,28 @@
+<?php
+namespace StellarWP\WPTables;
+
+class Container extends \tad_DI52_Container {
+	/**
+	 * Container instance.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var Container
+	 */
+	protected static $instance;
+
+	/**
+	 * Initialize the container.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return Container
+	 */
+	public static function init() {
+		if ( null === static::$instance ) {
+			static::$instance = new self;
+		}
+
+		return static::$instance;
+	}
+}
